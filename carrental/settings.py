19a7adb3565da6875fed48a2163d6e9d55e5d658
ASHUTOSH RANJAN <112747454+alwaysashutosh@users.noreply.github.com>
2024-06-17 19:06:50 +0530

    Update settings.py

--- carrental/settings.py
+++ carrental/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-
+#added manually
 STATIC_URL = 'static/'
 STATICFILES_DIRS=os.path.join(BASE_DIR / "static"),
 STATIC_ROOT = os.path.join(BASE_DIR / "staticfiles_build" ,'static')
@@ -143,7 +143,4 @@ STATIC_ROOT = os.path.join(BASE_DIR / "staticfiles_build" ,'static')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-#added manually
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR / "static")
-]
+
